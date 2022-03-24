7d2cc1b849cbd7a87b84ec020664d28276eba31c
CodeGuyUg <86480489+jumatech78@users.noreply.github.com>
2022-03-24 16:09:12 +0300

    updates on drawer

--- payment_system/src/components/Drawerbar/index.js
+++ payment_system/src/components/Drawerbar/index.js
@@ -1,11 +1,23 @@
 import React from "react";
 import "./drawerbarstyles.css";
+import AccountBalanceIcon from "@mui/icons-material/AccountBalance";
+import Badge from "@mui/material/Badge";
 
 function Drawerbar() {
   return (
-    <div className="drawerbar">
-      <div className="drawer_image">
-        <img src="/images.png" alt="" />
+    <div>
+      <div className="drawerbar">
+        <div className="drawer_header">
+          <img className="drawer_image" src="/images.png" alt="" />
+        </div>
+        <div>
+          <h2>Nexon</h2>
+        </div>
+      </div>
+      <div className="drawer_section">
+        <AccountBalanceIcon className="drawer_icon" />
+        <h4>Transactions</h4>
+        <Badge className="badge" badgeContent={2} badgeSize={15} color='primary'/>
       </div>
     </div>
   );
